0655f6b3e9cae8b59d4450c3433eedfc6e9b8b46
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2024-11-13 15:32:49 +0100

    Version 0.8.3

--- libreria.js
+++ libreria.js
@@ -10,7 +10,16 @@ fetch("https://striveschool-api.herokuapp.com/books")
 
     const riga = document.getElementById("riga-libri");
 
-    for (let i = 0; i < books.length; i++) {}
+    books.forEach((book) => {
+      riga.innerHTML += `<div class="card" style="width: 18rem;">
+      <img src="${book.img}" class="card-img-top mt-3" alt="${book.title}" style = "height: 390px">
+      <div class="card-body">
+        <h5 class="card-title">${book.title}</h5>
+        <p class="card-text">Price: $${book.price}</p>
+        <p class="card-text">Category: ${book.category}</p>
+      </div>
+    </div>`;
+    });
   })
   .catch((error) => {
     console.log(error);
